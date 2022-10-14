f7f3f061d2db731a14a8211d28943b39d01c5e1d
alexandrebelem21 <alexandrebelem21@gmail.com>
2022-10-14 15:58:23 -0300

    Initial commit

---
+++ storybook-static/assets/preview.8f7a5429.js
@@ -0,0 +1,2 @@
+import{t as o}from"./index.d565fe11.js";import"./iframe.4270acc5.js";import"./es.map.constructor.934e6520.js";import"./es.number.to-fixed.e5fa8f9c.js";import"./index.d9491d98.js";const s={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},docs:{them:o.dark}};export{s as parameters};
+//# sourceMappingURL=preview.8f7a5429.js.map
